cca8a65fefe6494e2cff400600d555d0e882acec
Tanmay <tanmaygangwar7@gmail.com>
2023-10-19 21:00:29 +0530

    Program to check whether array is sorted or not

--- ProblemsOnRecursion/CheckSorted.java
+++ ProblemsOnRecursion/CheckSorted.java
@@ -1,6 +1,27 @@
 // Program to check whether an array is sorted or not, using recursion
 package ProblemsOnRecursion;
 
-public class CheckSorted {
+import java.util.Scanner;
 
+public class CheckSorted {
+static boolean check(int a[], int ind)
+{
+    if(ind==a.length)
+    {
+        return false;
+    }
+
+    if(a[ind]<a[ind+1])
+    {
+        return true;
+    }
+    return check(a,ind);
+}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] a = {9,2,3,4,5};
+
+        System.out.println(check(a,0));
+    }
 }
