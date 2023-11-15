32d9375191f367acceb41eeb63a53ea0454ba28d
Tanmay <tanmaygangwar7@gmail.com>
2023-11-15 20:34:39 +0530

    Program to check palindrome

--- ProblemsOnRecursion/Strings/checkPalindrome.java
+++ ProblemsOnRecursion/Strings/checkPalindrome.java
@@ -1,22 +1,32 @@
 // Problem to check whether a string is palindrome
 package ProblemsOnRecursion.Strings;
 
+import java.util.Scanner;
+
 public class checkPalindrome {
     static boolean check(String str, int start, int end)
     {
-        if(start<end)
-            return false;
+        if(start>=end)
+            return true;
 
         if(str.charAt(start)!=str.charAt(end))
         {
             return false;
         }
-        else {
-            return check(str, start + 1, end - 1);
-        }
+        return check(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
-        System.out.println(check("level",0,4));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any string : ");
+        String s = sc.nextLine();
+        if(check(s,0,s.length()-1))
+        {
+            System.out.println(s+" is a palindrome string.");
+        }
+        else
+        {
+            System.out.println(s+" is not a palindrome string.");
+        }
     }
 }
